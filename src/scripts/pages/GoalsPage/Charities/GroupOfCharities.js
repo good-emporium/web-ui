9dd88b8b8c8b4529a64973661b3767a4a93c7f48
Dashiel Lopez Mendez <lopez.mendez.d@gmail.com>
2018-09-18 18:44:37 -0500

    Fetch orgs from api (#17)

--- src/scripts/pages/GoalsPage/Charities/GroupOfCharities.js
+++ src/scripts/pages/GoalsPage/Charities/GroupOfCharities.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
-import IndividualCharity from './IndividualCharity';
-import axios from 'axios'
+import { Link, Route } from 'react-router-dom';
+import CharityPage from '../../CharityPage/Charity';
+
 
 class GroupOfCharities extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      users: [],
-      store: []
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  } 
+  componentDidMount() {
+    fetch("https://api.goodemporium.com/v0/organizations")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.items
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="orgs-table d-flex flex-row flex-wrap">
+            {items.map(item => (
+              <div className="org-card">
+                <img src={item.logo} alt='CharityLogo' />
+                <Link to={'/org/'}>
+                <div>{item.name}</div>
+                </Link>
+                <p>{item.description}</p>
+                <Route path="/org/" component={CharityPage} />
+              </div>
+            ))}
+        </div>
+      );
     }
   }
-  componentDidMount() {
-    axios.get('https://randomuser.me/api/?results=10&inc=name,registered&nat=fr')
-      .then(json => json.data.results.map(result => (
-        {
-          name: `${result.name.first} ${result.name.last}`,
-          id: result.registered
-        })))
-      .then(newData => this.setState({ users: newData, store: newData }))
-      .catch(error => alert(error))
-  }
-
-  render() {
-    return (
-      <div className="orgs-table d-flex flex-row flex-wrap">
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-        <IndividualCharity />
-      </div>
-    );
-  }
-};
+}
 
 export default GroupOfCharities;
