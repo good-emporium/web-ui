8823cd8aa94c60dc17e53062cca25d25eff1441e
Jen Udan <jen.udan@gmail.com>
2018-09-20 17:04:34 -0500

    Style updates to homepage based on feedback (#18)
    * Fetch organizations from api endpoint and render on organizations page
    
    * Styled homepage based on feedback

--- src/scripts/components/Logo.js
+++ src/scripts/components/Logo.js
@@ -3,10 +3,11 @@ import { Link, Route } from 'react-router-dom';
 
 const logo = () => {
     return (
-        <div className="d-inline col">
+        <div className="d-inline col" id="logoContainer">
             
                 <Link to={'/'}>
-                    <img src="./../assets/img/good_emporium_logo_color.svg" />
+                    <img className="ge-logo" src="./../assets/img/good_emporium_logo_color.svg" />
+                     <div className="small-text tagline"> where <span className="ge-blue-bold">good</span> goes to work</div>
                 </Link>
             
         </div>
--- src/scripts/pages/HomePage/Goals/PicGoals.js
+++ src/scripts/pages/HomePage/Goals/PicGoals.js
@@ -4,94 +4,94 @@ import GoalsPage from '../../GoalsPage/Goals';
 
 const picGoals = () => {
         return (
-                <div className='icon-box d-flex flex-column  my-3'>
+                <div className='icon-box d-flex flex-column my-3' id="goalButtonsContainer">
                         <div className="ge-blue">
-                                <p className=' text-center my-0'>BROWSE BY UN GLOBAL GOALS</p>
+                                <p className='text-right my-0' id="goalButtonsTitle">BROWSE BY UN GLOBAL GOALS</p>
                         </div>
-                        <div className="d-flex flex-wrap">
+                        <div className="d-flex flex-wrap justify-content-end">
 
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/poverty'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-01.png" alt="" />
                                         </Link>
                                 </div>
 
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/zeroHunger'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-02.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/goodHealth'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-03.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/qualityEducation'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-04.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/genderEquality'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-05.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/cleanWater'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-06.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/cleanEnergy'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-07.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/decentWork'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-08.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons">
+                                <div className="goal-button">
                                         <Link to={'/goal/industry'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-09.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/reducedInequalities'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-10.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/sustainableCities'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-11.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/responsibleConsumption'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-12.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/clientAction'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-13.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/lifeBelowWater'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-14.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/lifeOnLand'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-15.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/peaceJustice'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-16.png" alt="" />
                                         </Link>
                                 </div>
-                                <div className="goal-buttons" >
+                                <div className="goal-button" >
                                         <Link to={'/goal/partnershipForTheGoals'}>
                                                 <img className='img-fluid' src="./../../../assets/img/E_SDG goals_icons-individual-rgb-17.png" alt="" />
                                         </Link>
