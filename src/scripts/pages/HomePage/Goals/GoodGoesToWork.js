0078808aaa4b5ec810fc4fa43958e4bd217bc2bf
Jen Udan <jen.udan@gmail.com>
2018-09-20 16:52:10 -0500

    Styled homepage based on feedback

--- src/scripts/pages/HomePage/Goals/GoodGoesToWork.js
+++ src/scripts/pages/HomePage/Goals/GoodGoesToWork.js
@@ -3,23 +3,14 @@ import React from 'react';
 const goodGoesToWork = () => {
     return (
         <div className='d-flex flex-column box1 text-left my-5'>
-            <div >
-                <div >
-                    <h2 className="large-text"> where <span className="ge-blue-bold">good</span>  <br /> goes to work </h2>
-                </div>
+            <div className='my-3'>
                 <div className="medium-text">
-                    <p>Search for your <span className="ge-blue-bold">cause</span></p>
-                    <p>Choose your <span className="ge-blue-bold">area.</span></p>
-                    <p>Do some <span className="ge-blue-bold">good.</span></p>
-                </div>
-                <div className='my-3'>
-                    <div className="medium-text">
-                        <p>Thousands of organizations are doing good work in your area, but finding which ones need your help can be tough. That's why the  <span className="ge-blue"> <strong>good</strong>emporium </span> was created.</p><br />
-                        <p>Connected with<a href="http://SocialGood.US"> SocialGood.US</a> and the United Nations <a href="https://www.globalgoals.org/">Sustainable Development Goals</a>,<span className="ge-blue"><strong> good</strong>emporium</span> helps good people do good.</p>
-                    </div>
+                    <p>Thousands of organizations are doing good work in your area, but finding which ones need your help can be tough. That's why  <span className="ge-blue"> <strong>good</strong>emporium </span> was created.</p>
+                    <p>Connected with<a href="http://SocialGood.US"> SocialGood.US</a>, <span className="ge-blue"><strong> good</strong>emporium</span> helps good people do good.</p>
+                    <p>Search organizations by selecting a global goal or typing with a keyword in the serach bar.</p>
+
                 </div>
             </div>
-
         </div>
     )
 
