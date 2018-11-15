fca451f110dcf01ab1ffa28bbcbbb0b1b0b6ef26
Carlos Alpuche <calpuche@stedwards.edu>
2018-11-14 18:53:42 -0600

    Made tiny changes
    -Updated Footer
    -Updated Registration Page

--- src/scripts/components/Footer/ContactInfo.js
+++ src/scripts/components/Footer/ContactInfo.js
@@ -5,8 +5,7 @@ const contactInfo = () => {
         <div className="container">
             <p className="no-margin-padding"> 5540 N Lamar Blvd</p>
             <p className="no-margin-padding">Austin, TX 78751</p>
-            <p className="no-margin-padding">(512)761-3842</p>
-            <p className="no-margin-padding"><a href="mailto:help@a+sg.com"><span className="ge-red">help@a+sg.com</span></a></p>
+            <p className="no-margin-padding"><a href="mailto:info@socialgood.us"><span className="ge-red">info@socialgood.us</span></a></p>
             <br />
         </div>
     )
