4f7d37138a544ebe69a3df79a42a7f92ecea6605
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2018-12-04 10:10:04 -0500

    Pass classification in Link component

--- src/scripts/pages/GoalsPage/Charities/CharityTypeMenu.js
+++ src/scripts/pages/GoalsPage/Charities/CharityTypeMenu.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const charityTypeMenu = () => {
     return (
         <div className="container">
             <div className="row">
-                <a href="#">
+                <Link to={{ pathname: '#all', state: { classification: 'All' }} }>
                     <p className='org-choice p-2'><u>All</u></p>
-                </a>
-                <a href="#">
+                </Link>
+                <Link to={{ pathname: '#non-profit', state: { classification: 'Non-profit' }} }>
                     <p className='org-choice p-2'>non-profit</p>
-                </a>
-                <a href="#">
+                </Link>
+                <Link to={{ pathname: '', state: { classification: 'For-profit' }} }>
                     <p className='org-choice p-2'>for profit</p>
-                </a>
-                <a href="#">
+                </Link>
+                <Link to={{ pathname: '', state: { classification: 'Government-org' }} }>
                     <p className='org-choice p-2'>government</p>
-                </a>
+                </Link>
             </div>
         </div>
     )
