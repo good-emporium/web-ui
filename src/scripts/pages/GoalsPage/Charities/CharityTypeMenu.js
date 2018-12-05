5629516bc7f4e39bc8c55375bf803dc4ab0acbe0
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2018-12-04 23:29:46 -0500

    Fix routing for classifications

--- src/scripts/pages/GoalsPage/Charities/CharityTypeMenu.js
+++ src/scripts/pages/GoalsPage/Charities/CharityTypeMenu.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-const charityTypeMenu = () => {
+const charityTypeMenu = withRouter((props) => {
+    const { url } = props.match
+    const currentPath = props.location.pathname.split('/')[2];
     return (
         <div className="container">
             <div className="row">
-                <Link to={{ pathname: '#all', state: { classification: 'All' }} }>
+                <Link to={{ pathname: `${url}/${currentPath}/all`, state: { classification: 'All' }} }>
                     <p className='org-choice p-2'><u>All</u></p>
                 </Link>
-                <Link to={{ pathname: '#non-profit', state: { classification: 'Non-profit' }} }>
+                <Link to={{ pathname: `${url}/${currentPath}/non-profit`, state: { classification: 'Non-profit' }} }>
                     <p className='org-choice p-2'>non-profit</p>
                 </Link>
-                <Link to={{ pathname: '', state: { classification: 'For-profit' }} }>
+                <Link to={{ pathname: `${url}/${currentPath}/for-profit`, state: { classification: 'For-profit' }} }>
                     <p className='org-choice p-2'>for profit</p>
                 </Link>
-                <Link to={{ pathname: '', state: { classification: 'Government-org' }} }>
+                <Link to={{ pathname: `${url}/${currentPath}/government`, state: { classification: 'Government-org' }} }>
                     <p className='org-choice p-2'>government</p>
                 </Link>
             </div>
         </div>
     )
-
-
-};
+});
 
 export default charityTypeMenu;
