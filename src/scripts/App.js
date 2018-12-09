7a8c8bc701fd591fd2a6d71d1d678db7653db485
Carlos Alpuche <calpuche@stedwards.edu>
2018-12-09 11:50:43 -0600

    Bug Fix Update
    -Update To Include div in between routers

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
   Route,
   BrowserRouter
-} from "react-router-dom";import HomePage from './pages/HomePage/HomePage';
+} from "react-router-dom"; import HomePage from './pages/HomePage/HomePage';
 import GoalsPage from './pages/GoalsPage/Goals';
 import CharityPage from './pages/CharityPage/Charity';
 import LoginPage from './pages/LoginPage/Form';
@@ -16,7 +16,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       error: null,
       isLoaded: false,
       items: localStorage.getItem('organizations'),
@@ -31,34 +31,36 @@ class App extends Component {
       .then(response => response.json())
       .then(
         (jsonData) => {
-           var JSONObject = [];
-            for(var i in jsonData){
-              var key = i;
-              var val = jsonData[i];
-              for(var j in val){
-                  var sub_key = j;
-                  var sub_val = val[j];
-                  JSONObject[j]=val[j];
-              }
+          var JSONObject = [];
+          for (var i in jsonData) {
+            var key = i;
+            var val = jsonData[i];
+            for (var j in val) {
+              var sub_key = j;
+              var sub_val = val[j];
+              JSONObject[j] = val[j];
+            }
           }
-          
+
           localStorage.setItem('organizations', JSON.stringify(JSONObject))
-            this.setState({ items: localStorage.getItem(JSONObject)});
-      })
+          this.setState({ items: localStorage.getItem(JSONObject) });
+        })
   }
 
   render() {
     return (
-        <div className="container" id="app">
+      <div className="container" id="app">
         <BrowserRouter>
-          <Route exact={true} path="/" component={HomePage} />
-          <Route path="/goal/" component={GoalsPage} />
-          <Route path="/org/" component={CharityPage} />
-          <Route exact={true} path="/login/" component={LoginPage} />
-          <Route exact={true} path="/register/" component={RegisterPage} />
-          <Route exact={true} path="/aboutUs/" component={AboutUsPage} />
-          <Route exact={true} path="/Support/" component={Support} />
-      </BrowserRouter>
+          <div className="Content">
+            <Route exact={true} path="/" component={HomePage} />
+            <Route path="/goal/" component={GoalsPage} />
+            <Route path="/org/" component={CharityPage} />
+            <Route exact={true} path="/login/" component={LoginPage} />
+            <Route exact={true} path="/register/" component={RegisterPage} />
+            <Route exact={true} path="/aboutUs/" component={AboutUsPage} />
+            <Route exact={true} path="/Support/" component={Support} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
