25ebf55eda4dc3c311361830f8ec677a4355c824
Carlos Alpuche <calpuche@stedwards.edu>
2019-02-26 19:57:26 -0600

    Registration page update (#35)
    * Small Update
    
    -Switched Zip to Postal
    
    * Update Registration Form
    
    -Updated to use Bootstrap
    -Updated How  it is organized
    -Added Organization Section

--- src/scripts/pages/RegisterPage/Form.js
+++ src/scripts/pages/RegisterPage/Form.js
@@ -3,78 +3,116 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 const form = () => {
     return (
-        <div className="container-fluid" id='registerPage'>
+        <div id='registerPage'>
             <Header />
             <form>
                 <h3>Sign Up</h3>
                 <p>Create your Good Emporium Account:</p>
-                <div className="row">
-                    <div className="col-6">
-                    <div className="container">
-                        <div className="form-group">
-                            <label className="animated-label" htmlFor="email"><b>Email:</b></label>
-                            <input type="text" className="text-line"  name="email" required />
-                        </div>
-                        <div className="form-group">
-                            <label className="animated-label" htmlFor="username"><b>Username:</b></label>
-                            <input type="text" className="text-line"  autoComplete="username" name="username" required />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="psw"><b>Password:</b></label>
-                            <input type="password" className="text-line"  name="psw" autoComplete="new-password" required />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="psw-repeat"><b>Repeat Password:</b></label>
-                            <input type="password" className="text-line"  name="psw-repeat" autoComplete="new-password" required />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="firstName"><b>First Name:</b></label>
-                            <input type="text" className="text-line"  name="firstName" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="lastName"><b>Last Name:</b></label>
-                            <input type="text" className="text-line"  name="lastName" />
-                        </div>
+                <h4>User Info: </h4>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label className="animated-label" htmlFor="email"><b>Email:</b></label>
+                        <input type="text" className="form-control" name="email" placeholder="Email" required />
                     </div>
+                    <div className="form-group col-md-6">
+                        <label className="animated-label" htmlFor="username"><b>Username:</b></label>
+                        <input type="text" className="form-control" autoComplete="username" name="username" placeholder="Username" required />
                     </div>
-                    <div className="column">
-                        <div className="form-group">
-                            <label htmlFor="address"><b>Address:</b></label>
-                            <input type="text" className="text-line"  name="address" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="city"><b>City:</b></label>
-                            <input type="text" className="text-line"  name="city" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="regionState"><b>State or Region:</b></label>
-                            <input type="text" className="text-line"  name="regionState" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="country"><b>Country:</b></label>
-                            <input type="text" className="text-line"  name="country" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="zipCode"><b>Zip Code:</b></label>
-                            <input type="text" className="text-line"  name="zipCode" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phoneNumber"><b>Phone Number:</b></label>
-                            <input type="text" className="text-line"  name="phoneNumber" />
-                        </div>
-                        
-                        </div>
-                        
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="bio"><b>Bio:</b></label>
-                            <input type="text" className="text-line"  name="bio" />
-                        
-                        
-                        </div>
-                        <button className="btn btn-outline-primary" type="submit">Sign Up</button>
-                    
-                
+                    <div className="form-group col-md-6">
+                        <label htmlFor="psw"><b>Password:</b></label>
+                        <input type="password" className="form-control" name="psw" autoComplete="new-password" placeholder="Password" required />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="psw-repeat"><b>Repeat Password:</b></label>
+                        <input type="password" className="form-control" name="psw-repeat" autoComplete="new-password" placeholder="Password" required />
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="title">Title:</label>
+                        <select id="title" class="form-control">
+                            <option selected>...</option>
+                            <option>Mr.</option>
+                            <option>Mrs.</option>
+                            <option>Miss</option>
+                            <option>Ms</option>
+                            <option>Dr.</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-md-5">
+                        <label htmlFor="firstName"><b>First Name:</b></label>
+                        <input type="text" className="form-control" name="firstName" placeholder="First Name" required />
+                    </div>
+                    <div className="form-group col-md-5">
+                        <label htmlFor="lastName"><b>Last Name:</b></label>
+                        <input type="text" className="form-control" name="lastName" placeholder="Last Name" required />
+                    </div>
+                    <div className="form-group col-md-6" >
+                        <label htmlFor="address"><b>Address:</b></label>
+                        <input type="text" className="form-control" name="address" placeholder="1234 Main St" required />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label for="inputAddress2">Address 2</label>
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" required />
+                    </div>
+
+                    <div className="form-group col-md-6">
+                        <label htmlFor="city"><b>City:</b></label>
+                        <input type="text" className="form-control" name="city" placeholder="City" required />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="regionState"><b>State or Region:</b></label>
+                        <input type="text" className="form-control" name="regionState" placeholder="State or Region" />
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label htmlFor="postalCode"><b>Postal Code:</b></label>
+                        <input type="text" className="form-control" name="postalCode" placeholder="Postal Code" required />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="country"><b>Country:</b></label>
+                        <input type="text" className="form-control" name="country" placeholder="Country" required />
+                    </div>
+
+                    <div className="form-group col-md-6">
+                        <label htmlFor="phoneNumber"><b>Phone Number:</b></label>
+                        <input type="text" className="form-control" name="phoneNumber" placeholder="Phone Number" required />
+                    </div>
+                </div>
+
+
+                <h4>Company Information:</h4>
+                <div className="form-row">
+                    <div className="form-group col-md-6" >
+                        <label htmlFor="organizationName"><b>Organization Name:</b></label>
+                        <input type="text" className="form-control" name="organizationName" placeholder="Organization Name" required />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="website"><b>Website:</b></label>
+                        <input type="text" className="form-control" name="website" placeholder="Website" required/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="organizationEmailAddress"><b>Organization Email Address:</b></label>
+                        <input type="text" className="form-control" name="organizationEmailAddress" placeholder="Organization Email Address" required/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="einNumber"><b>EIN Number:</b></label>
+                        <input type="text" className="form-control" name="einNumber" placeholder="EIN Number" required/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="addressOfOrganization"><b>Address of Organization:</b></label>
+                        <input type="text" className="form-control" name="addressOfOrganization" placeholder="Address of Organization" required/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="subsidiariesAffiliate"><b>Subsidiaries/Affiliate:</b></label>
+                        <input type="text" className="form-control" name="subsidiariesAffiliate" placeholder="Subsidiaries/Affiliate" required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="notes"><b>Notes:</b></label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"  name="notes" placeholder="Notes/Comments"></textarea>
+                    </div>
+                </div>
+
+
+                <button className="btn btn-outline-primary" type="submit">Next</button>
+
             </form>
             <Footer />
         </div>
