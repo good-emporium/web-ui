49c771cfe986738c5a991954c61eb6eb2a0584e9
Carlos Alpuche <calpuche@stedwards.edu>
2019-03-11 17:35:30 -0500

    Updated Login Page
    -Updated Login Page to use Bootstrap
    -Updated Packages

--- src/scripts/pages/LoginPage/Form.js
+++ src/scripts/pages/LoginPage/Form.js
@@ -9,11 +9,11 @@ const form = () => {
                 <h1>Sign In</h1>
                 <div className="form-group">
                     <label htmlFor="username"><b>Username:</b></label>
-                    <input type="text" className="text-line"  name="username" required />
+                    <input type="text" className="form-control"  name="username" placeholder="Username" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="psw"><b>Password:</b></label>
-                    <input type="password" className="text-line"  name="psw" required />
+                    <input type="password" className="form-control"  name="psw" placeholder="Password" required />
                </div>
                 <button className="btn btn-outline-primary" type="submit">Sign In</button>
             </form>
